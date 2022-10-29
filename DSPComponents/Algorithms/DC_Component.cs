82c6f7d5fd9d5ff4956757c2d8b24e50fc1caf30
unknown <rewanrrr61@gmail.com>
2022-10-29 13:04:46 +0200

    DC

---
+++ DSPComponents/Algorithms/DC_Component.cs
@@ -0,0 +1,33 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+
+    public class DC_Component: Algorithm
+    {
+        float mean = 0f;
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            List<float> arr = new List<float>();
+            OutputSignal = new Signal(arr, false);
+            for (int i=0; i<InputSignal.Samples.Count; i++)
+            {
+                 mean = mean + InputSignal.Samples[i];
+            }
+            mean = mean / InputSignal.Samples.Count;
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                float sig = InputSignal.Samples[i] - mean;
+                OutputSignal.Samples.Add(sig);
+            }
+        }
+    }
+}
---
+++ DSPComponents/Algorithms/SinCos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class SinCos : Algorithm
+    {
+        double oo;
+        public string type { get; set; }
+        public float A { get; set; }
+        public float PhaseShift { get; set; }
+        public float AnalogFrequency { get; set; }
+        public float SamplingFrequency { get; set; }
+        public List<float> samples { get; set; }
+        public Signal OutputSignal { get; set; }
+        public override void Run()
+        {
+            List<float> arr = new List<float>();
+            OutputSignal = new Signal(arr, false);
+            if (type == "sin")
+            {
+                for (int i = 0; i < samples.Count; i++)
+                {
+                    oo = A *( Math.Sin((2 * 180*(AnalogFrequency/SamplingFrequency)*i)+PhaseShift));
+                    OutputSignal.Samples.Add(Convert.ToSingle(oo));
+
+                }
+                
+            }
+            else
+            {
+                for (int i = 0; i < samples.Count; i++)
+                {
+                    oo = A *( Math.Cos((2 * 180 * (AnalogFrequency / SamplingFrequency) * i) + PhaseShift));
+                    OutputSignal.Samples.Add(Convert.ToSingle(oo));
+                   
+                }
+            }
+        }
+    }
+}
