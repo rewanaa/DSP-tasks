0c81c08067e22282e5c61900bf49c817cca670fa
unknown <rewanrrr61@gmail.com>
2022-10-30 20:56:42 +0200

    sincos

--- DSPComponents/Algorithms/SinCos.cs
+++ DSPComponents/Algorithms/SinCos.cs
@@ -10,37 +10,37 @@ namespace DSPAlgorithms.Algorithms
 {
     public class SinCos : Algorithm
     {
-        double oo;
         public string type { get; set; }
         public float A { get; set; }
         public float PhaseShift { get; set; }
         public float AnalogFrequency { get; set; }
         public float SamplingFrequency { get; set; }
         public List<float> samples { get; set; }
-        public Signal OutputSignal { get; set; }
-        public override void Run()
+        //public Signal OutputSignal { get; set; }
+        public override void Run() 
         {
-            List<float> arr = new List<float>();
-            OutputSignal = new Signal(arr, false);
+            samples = new List<float>();
+            float oo;
             if (type == "sin")
             {
-                for (int i = 0; i < samples.Count; i++)
+                for (int i = 0; i <SamplingFrequency; i++)
                 {
-                    oo = A *( Math.Sin((2 * 180*(AnalogFrequency/SamplingFrequency)*i)+PhaseShift));
-                    OutputSignal.Samples.Add(Convert.ToSingle(oo));
+                    oo =(float)Math.Round((float)( A *( Math.Sin((2 * Math.PI*(AnalogFrequency/SamplingFrequency)*i)+PhaseShift))),6);
+                    samples.Add(oo);
 
                 }
                 
             }
             else
             {
-                for (int i = 0; i < samples.Count; i++)
+                for (int i = 0; i < SamplingFrequency; i++)
                 {
-                    oo = A *( Math.Cos((2 * 180 * (AnalogFrequency / SamplingFrequency) * i) + PhaseShift));
-                    OutputSignal.Samples.Add(Convert.ToSingle(oo));
-                   
+                    oo = (float)Math.Round((float)(A * (Math.Cos((2 * Math.PI * (AnalogFrequency / SamplingFrequency) * i) + PhaseShift))), 6);
+                    samples.Add(oo);
+
                 }
             }
+            
         }
     }
 }
