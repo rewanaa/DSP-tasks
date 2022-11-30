45148da0b046a94b517272318de469606a759827
unknown <rewanrrr61@gmail.com>
2022-11-30 16:53:51 +0200

    DCT

--- DSPComponents/Algorithms/DCT.cs
+++ DSPComponents/Algorithms/DCT.cs
@@ -16,18 +16,20 @@ namespace DSPAlgorithms.Algorithms
         {
             List<float> dc = new List<float>();
             float r = 0f;
-            //Console.Write(InputSignal.Samples.Count);
+            
             for (int k = 0; k <= InputSignal.Samples.Count; k++)
             {
                 for (int n = 0; n < InputSignal.Samples.Count; n++)
                 {
-                    r = InputSignal.Samples[n] * (float)Math.Cos((Math.PI / (4 * InputSignal.Samples.Count)) * ((2 * n) - 1) * ((2 *k) - 1));
-                    
-                    r += r;
+                   
+                    r += InputSignal.Samples[n] * ((float)Math.Cos(((float)Math.PI / (4.0 * InputSignal.Samples.Count)) * ((2 * n) - 1) * ((2 *k) - 1)));
+                  //  Console.Write(r + " ");
+
                 }
+                //Console.Write(InputSignal.Samples.Count);
                 //Console.Write(r + " ");
-                Console.Write(((float)Math.Sqrt(2 / InputSignal.Samples.Count)) * r+"  ");
-                dc.Add(((float)Math.Sqrt(2/ InputSignal.Samples.Count))*r);
+                Console.Write(((float)Math.Sqrt(2.0 / (float)InputSignal.Samples.Count)) * r + "  ");
+                dc.Add(((float)Math.Sqrt(2.0 / (float)InputSignal.Samples.Count)) * r);
             }
             OutputSignal = new Signal(dc, false);
         }
