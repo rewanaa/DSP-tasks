42bff7cbb96477df40ee88ceb690b1c554ad8c9b
Omar Ismail <omar_som3a2015@hotmail.com>
2022-12-06 12:55:32 +0200

    task 4 commit

--- DSPComponents/Algorithms/FastConvolution.cs
+++ DSPComponents/Algorithms/FastConvolution.cs
@@ -20,79 +20,7 @@ namespace DSPAlgorithms.Algorithms
         {
             List<float> outputSignal = new List<float>();
             List<int> index = new List<int>();
-            int convSignalSize = InputSignal1.Samples.Count + InputSignal2.Samples.Count - 1;
-            int min = InputSignal1.SamplesIndices[0] + InputSignal2.SamplesIndices[0];
-            int max = InputSignal1.SamplesIndices[InputSignal1.SamplesIndices.Count - 1] + InputSignal2.SamplesIndices[InputSignal2.SamplesIndices.Count - 1];
 
-            while (min <= max) { index.Add(min++); }
-            for (int i = 0; i < convSignalSize; i++)
-            {
-                float Signal, Musk, sum = 0;
-
-                for (int j = 0; j <= i; j++)
-                {
-                    if (j >= InputSignal1.Samples.Count)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        Signal = InputSignal1.Samples[j];
-                    }
-                    if (i - j >= InputSignal2.Samples.Count)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        Musk = InputSignal2.Samples[i - j];
-                    }
-
-                    sum += Signal * Musk;
-                }
-                outputSignal.Add(sum);
-            }
-            int zeros = 0;
-            for (int i = 0; i < outputSignal.Count; i++)
-            {
-                if (outputSignal[i] != 0)
-                {
-                    break;
-                }
-                else
-                {
-                    zeros++;
-                }
-            }
-            for (int i = 0; i < zeros; i++)
-            {
-                outputSignal.RemoveAt(0);
-                index.RemoveAt(0);
-            }
-            zeros = 0;
-            for (int i = outputSignal.Count - 1; i >= 0; i--)
-            {
-                if (outputSignal[i] != 0)
-                {
-                    break;
-                }
-                else
-                {
-                    zeros++;
-                }
-            }
-            for (int i = 0; i < zeros; i++)
-            {
-                outputSignal.RemoveAt(outputSignal.Count - 1);
-                index.RemoveAt(index.Count - 1);
-            }
-
-
-
-
-
-
-            OutputConvolvedSignal = new Signal(outputSignal, index, false);
         }
     
 }
